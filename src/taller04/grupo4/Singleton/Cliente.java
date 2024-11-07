a6d8bc4d3af41318cdd0be9d3278a963413beab2
Paulo Tapia <pmtapia@espol.edu.ec>
2024-11-06 22:00:21 -0500

    ...
    orden

--- src/taller04/grupo4/Singleton/Cliente.java
+++ src/taller04/grupo4/Singleton/Cliente.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package taller04.grupo4;
+package taller04.grupo4.Singleton;
 
 /**
  *
